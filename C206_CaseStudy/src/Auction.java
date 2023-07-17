372f8b7cefc64ac9ef2578c48e9db7e49fc65d7d
22012269 <22012269@10.175.3.136>
2023-07-17 18:09:33 +0800

    Auction Class

--- C206_CaseStudy/src/Auction.java
+++ C206_CaseStudy/src/Auction.java
@@ -1,4 +1,31 @@
 
 public class Auction {
+	private String title;
+	private String description;
+	private String startTime;
+	private String endTime;
+	
+	public Auction(String title, String description, String startTime, String endTime) {
+		this.title = title;
+		this.description = description;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
 }
+

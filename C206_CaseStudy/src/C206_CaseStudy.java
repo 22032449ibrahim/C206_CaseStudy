f9d4ddec40388e0ccd6f8d6645da273c62f63abd
ibrah <ibrah@DESKTOP-Q2JVIIB>
2023-07-28 21:13:58 +0800

    Updated classes changed datetime back to string. Reorganized main method

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,467 +1,172 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
-
+	
+	private static final int OPTION_ADD_PAYMENT = 14;
+	private static final int OPTION_VIEW_PAYMENT = 15;
+	private static final int OPTION_DELETE_PAYMENT = 16;
 	public static void main(String[] args) {
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//view options 
-		
-		// View all Users
+		// view options
+		int option = 0;
+		while (option != 5) {
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// View all auctions
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
 
-		
-		
-		
-		
-		
-		
-		
-		// View all items
+			if (option == 1) {
+				// Add user
 
-		
-		
-		
-		
-		
-		
-		
-		
-		// View all bids
+			} else if (option == 2) {
+				// View all user
 
-		
-		
-		
-		
-		
-		
-		
-		
-		// View all payments
+			} else if (option == 3) {
+				// Delete existing user
 
-		
-		
-		
-		
-		
-		
-		
-		
+			} else if (option == 4) {
+				// Add auction
 
-		
-		
-		
-		
-		
-		
-		
-		// Add options
-		
-		// Add Users
+			} else if (option == 5) {
+				// View all auction
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Add auctions
+			} else if (option == 6) {
+				// Delete existing auction
 
-		
-		
-		
-	 
-		
-		
-		
-		
-		
-		// Add items
+			} else if (option == 7) {
+				// Add item
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Add bids
+			} else if (option == 8) {
+				// View all item
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Add payments
+			} else if (option == 9) {
+				// Delete existing item
 
-		
-		
-		
-		
-		
-		
-		// Delete Users
+			} else if (option == 10) {
+				// Add bid
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Delete auction
+			} else if (option == 11) {
+				// View all bids
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Delete items
+			} else if (option == 12) {
+				// Delete existing bid
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Delete bids
+			} else if (option == OPTION_ADD_PAYMENT) {
+				// Add payment
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Delete payments
+			} else if (option == OPTION_VIEW_PAYMENT) {
+				// View all payment
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// menu
-		
-		
-		
-		// set header
-		
-		//exist
-		
-		
-		
-		
-		
-		//================================= Option 1 Viewing (CRUD- Read) =================================
-		
-		// retrieve and view all Users
+			} else if (option == OPTION_DELETE_PAYMENT) {
+				// Delete payment
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// retrieve and view all auctions
+			} else if (option == 16) {
+				System.out.println("Bye!");
 
-				
-				
-				
-				
-				
-				
-				
-		// retrieve and view all items
+			} else {
+				System.out.println("Invalid option");
+			}
 
-				
-				
-				
-				
-				
-				
-				
-				
-		// retrieve and view all bids
+		}
+	}
 
-				
-				
-				
-				
-				
-				
-				
-				
-		// retrieve and view all payments
+	// menu
+	public static void menu() {
+		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
+		System.out.println("1. Display Inventory");
+		System.out.println("2. Add a new User");
+		System.out.println("3. View all Users");
+		System.out.println("4. Delete an existing Item");
+		System.out.println("5. Add a new Auction");
+		System.out.println("6. View all Auction");
+		System.out.println("7. Delete a existing Auction");
+		System.out.println("8. Add a new Item");
+		System.out.println("9. View all Items");
+		System.out.println("10. Delete an existing Item");
+		System.out.println("11. Add a new Bid");
+		System.out.println("12. View all Bids");
+		System.out.println("13. Delete an existing Bid");
+		System.out.println("14. Add a new Payment");
+		System.out.println("15. View all Payments");
+		System.out.println("16. Delete an existing Payment");
+		System.out.println("17. Quit");
+		Helper.line(80, "-");
 
-				
-				
-				
-				
-				
-				
-				
-				
+	}
 
-				
-				
-				
-				
-				
-				
-				
-		
-		//================================= Option 2 Add an item (CRUD - Create) =================================
-		
-		// Input and Add Users
+	// set header
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// Input and Add auctions
+	// exist
+	public static String showAvailability(boolean isAvailable) {
+		String avail;
 
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		// Input and Add items
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
 
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		// Input and Add bids
+	// ================================= Option 1 Viewing (CRUD- Read)=================================
 
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		// Input and Add payments
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//================================= Option 3 delete(CRUD - delete)=================================
-		
-		// exist and delete Users
+	// retrieve and view all Users
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// exist and delete auctions
+	// retrieve and view all auction
 
-						
-						
-						
-						
-						
-						
-						
-						
-						
-		// exist and delete items
+	// retrieve and view all items
 
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-		// exist and delete bids
+	// retrieve and view all bids
 
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-		// exist and delete payments
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+	// retrieve and view all payments
+	public static String retrieveAllPayments(ArrayList<Payment> camcorderList) {
+		String output = "";
+		return output;
+	}
+	
+	public static void viewAllPayments(ArrayList<Payment> camcorderList) {
+
+	}
+	// ================================= Option 2 Add an item (CRUD - Create)=================================
+
+	// Input and Add Users
+
+	// Input and Add auction
+
+	// Input and Add items
+
+	// Input and Add bids
+
+	// Input and Add payments
+	public static Payment inputPayment() {
+		return null;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	}
+	public static void addPayment(ArrayList<Payment> camcorderList, Payment cc) {
+
+	}
+	// ================================= Option 3 delete(CRUD - delete)=================================
+
+	// exist and delete Users
+
+	// exist and delete auction
+
+	// exist and delete items
+
+	// exist and delete bids
+
+	// exist and delete payments
+	public static boolean doReturnCamcorder(ArrayList<Payment> camcorderList,String tag) {
+		boolean isReturned = false;
+		return isReturned;
 		
 	}
 
+	public static void returnCamcorder(ArrayList<Payment> camcorderList) {
+		
+	}
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,57 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
 
+	private Payment p1;
+	private Payment p2;
+
+	private ArrayList<Payment> PaymentList;
+
+	public C206_CaseStudyTest() {
+		super();
+	}
+
 	@Before
 	public void setUp() throws Exception {
+		// prepare test data
+		p1 = new Payment("1234-1234-1234-1234", "Johnny Bob", "12/12/2022", 999.20);
+		p2 = new Payment("1234-1234-1234-4321", "Bobby Kim", "11/12/2022", 123.45);
+
+		PaymentList = new ArrayList<Payment>();
+	}
+
+	@Test
+	public void testAddPayment() {
+		// fail("Not yet implemented");
+		
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Check if there is valid Camcorder arraylist to add to", PaymentList);
+		
+		// Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		// The item just added is as same as the first item of the list
+		C206_CaseStudy.addPayment(PaymentList, p1);
+		assertEquals("Check that Payment arraylist size is 1", 1, PaymentList.size());
+		assertSame("Check that Camcorder is added", p1, PaymentList.get(0));
+
+		// Add another item. test The size of the list is 2? -normal
+		// The item just added is as same as the second item of the list
+		C206_CaseStudy.addPayment(PaymentList, p2);
+		assertEquals("Check that Payment arraylist size is 2", 2, PaymentList.size());
+		assertSame("Check that Payment is added", p2, PaymentList.get(1));
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
 
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+		p1 = null;
+		p2 = null;
+		PaymentList = null;
 	}
 
 }
